1bb6ea84848439dc3f57adfce977055d2f9c8075
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2011-12-16 09:23:42 +0100

    ICS fixes #2

---
+++ cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GN
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/pyramid/full_pyramid.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := pyramid
+PRODUCT_NAME := cm_pyramid
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := Sensation
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pyramid BUILD_ID=ITL41F BUILD_DISPLAY_ID=ITL41F BUILD_FINGERPRINT="htc_europe/htc_pyramid/pyramid:2.3.4/GRJ22/125597.2:user/release-keys" PRIVATE_BUILD_DESC="pyramid-user 4.0.1 ITL41F 228551 release-keys"
