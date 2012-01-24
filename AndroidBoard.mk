3cb834d46b4afd5e81c39cf5fbf8e423f95e7064
Holger Gilbergs <admin@mblaster.de>
2012-01-24 15:10:47 +0100

    Huawei mediapad (based on pyramid)

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -25,4 +25,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/htc/pyramid/BoardConfigVendor.mk
+-include vendor/huawei/mediapad/BoardConfigVendor.mk
